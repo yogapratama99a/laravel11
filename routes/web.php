04548b88aeb31e5a58e281faa7246a8027e8fb47
yogapratama <e41230467@polije.student.polije.ac.id>
2025-02-21 09:26:44 +0700

    perbaikan acara 8

--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,42 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ManagementUsercontroller;
+use App\Http\Controllers\ManagementUserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Frontend\HomeController;
 
-//Acara 3
+// Acara 3
 Route::get('/index', function () {
     return view('welcome');
 });
 Route::get('/user', [UserController::class, 'index']);
 
-Route::match(['get', 'post'], '/', function () {
-    return 'ini match';
-});
 Route::any('/', function () {
     return 'ini any';
 });
-Route::redirect('/here', 'there', 301);
+
+Route::redirect('/here', '/there', 301);
 Route::view('/welcome2', 'welcome');
 Route::view('/welcome3', 'welcome')->name('Taylor');
-Route::get('user/acara3/{name?}', function ($name = null) {
-    return $name;
-});
+
 Route::get('user/acara3/{name?}', function ($name = "John") {
     return $name;
-});
-Route::get('user/acara3/{name}', function ($name) {
-    return $name;
 })->where('name', '[A-Za-z]+');
+
 Route::get('user/{id}', function ($id) {
     return $id;
 })->where('id', '[0-9]+');
+
 Route::get('user/{id}/{name}', function ($id, $name) {
-    return "User ID: $id, Username: $nama";
- })->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+    return "User ID: $id, Username: $name";
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
 Route::get('search/{search}', function ($search) {
     return $search;
 })->where('search', '.*');
 
-
-//Acara 4
+// Acara 4
 Route::get('/user/{id}/profile', function ($id) {
     return "Profil user dengan ID $id";
 })->name('profile');
@@ -50,29 +45,30 @@ Route::get('/generate-url', function () {
     $url = route('profile', ['id' => 5]);
     return "URL ke profile: $url";
 });
+
 Route::get('/redirect-profile', function () {
     return redirect()->route('profile', ['id' => 5]);
 });
+
 Route::middleware(['first', 'second'])->group(function () {
-    Route::get('/first', function () {
-    });
-    Route::get('/user/profile', function () {
-    });
+    Route::get('/first', function () {});
+    Route::get('/user/profile', function () {});
 });
-Route::
-        namespace('Admin')->group(function () { });
-Route::domain('{account).myapp.com')->group(function () {
+
+Route::domain('{account}.myapp.com')->group(function () {
     Route::get('user/{id}', function ($account, $id) {
+        return "Akun: $account, ID: $id";
     });
 });
+
 Route::prefix('admin')->group(function () {
-    Route::get('users', function () {
-    });
+    Route::get('users', function () {});
 });
-Route::name('admin')->group(function () {
-    Route::get('users', function () {
-    })->name('users');
+
+Route::name('admin.')->group(function () {
+    Route::get('users', function () {})->name('users');
 });
+
 
 //Acara 5
 Route::get('user', [ManagementUserController::class, 'index']);
