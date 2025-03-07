2a95aab5d272be57d734bf503e66be91aaef34e8
yogapratama <e41230467@polije.student.polije.ac.id>
2025-03-07 10:42:05 +0700

    acara 22

--- resources/views/backend/layouts/template.blade.php
+++ resources/views/backend/layouts/template.blade.php
@@ -76,12 +76,12 @@
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                         <img src="{{ asset('niceadmin/assets/img/profile-img.jpg')}}" alt="Profile"
                             class="rounded-circle">
-                        <span class="d-none d-md-block dropdown-toggle ps-2">Alex Pramu Uy Uy</span>
+                        <span class="d-none d-md-block dropdown-toggle ps-2">Yoga Pratama</span>
                     </a><!-- End Profile Iamge Icon -->
 
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                         <li class="dropdown-header">
-                            <h6>Muhamad Igviloja Mahendra</h6>
+                            <h6>Yoga</h6>
                             <span>Developer</span>
                         </li>
                         <li>
